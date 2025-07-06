d22076904853e1784024db3c3d14bcb11c4a98b1
Jake Soucy <jakennik01@gmail.com>
2025-07-06 15:19:26 -0300

    Printing the Bingo Card Perfectly

--- app.py
+++ app.py
@@ -1,35 +1,31 @@
 import random
 
-bList = [random.randint(1, 15) for num in range(5)]
-iList = [random.randint(16, 30) for num in range(5)]
-nList = [random.randint(31, 45) for num in range(5)]
-gList = [random.randint(46, 60) for num in range(5)]
-oList = [random.randint(61, 75) for num in range(5)]
+bList = random.sample(range(1, 16), 5)
+iList = random.sample(range(16, 31), 5)
+nList = random.sample(range(31, 46), 5)
+gList = random.sample(range(46, 61), 5)
+oList = random.sample(range(61, 76), 5)
 
 def bottomTopLine():
+    print("*   *   *   *   *   *   *   *   *   *   *")
+
+def printBingoLineLess(count, bList, iList, nList, gList, oList):
+    print("*   " + str(bList[count]) + "   *   " + str(iList[count]) + "  *   " + str(nList[count]) + "  *   " + str(gList[count]) + "  *   " + str(oList[count]) + "  *")
+
+def printBingoLineMore(count, bList, iList, nList, gList, oList):
+     print("*  " + str(bList[count]) + "   *   " + str(iList[count]) + "  *   " + str(nList[count]) + "  *   " + str(gList[count]) + "  *   " + str(oList[count]) + "  *")
+
+def numberLine(bList, iList=None, nList=None, gList=None, oList=None):
     count = 0
-    line = ""
     while(count < 5):
-        line += "*   *   *   *   *   "
-        count += 1
-    print(line)
-
-def numberLine(listOfNums):
-    count = 0
-    line = ""
-    while(count < 5):
-        line += "*   " + str(listOfNums[count]) + "   "
-        count+= 1
-    print(line)
-
-
-
-
+            if (bList[count] < 10):
+                printBingoLineLess(count, bList, iList, nList, gList, oList)
+            else:
+                printBingoLineMore(count, bList, iList, nList, gList, oList)    
+            count+= 1
 
 bottomTopLine()
 
-numberLine(bList)
-
-
+numberLine(bList, iList, nList, gList, oList)
 
 bottomTopLine()
