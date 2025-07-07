85c403ad55627440cc2aede57abd2df9dc5b8c83
Jake Soucy <jakennik01@gmail.com>
2025-07-07 18:00:27 -0300

    User can now interact with program and make selected numbers green, thus marked, giving them the boolean of True

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import random
+import os
+import keyboard
+from colorama import init, Fore, Style
+import time
+init()  # Needed for Windows
 
 bList = random.sample(range(1, 16), 5)
 iList = random.sample(range(16, 31), 5)
@@ -6,26 +11,75 @@ nList = random.sample(range(31, 46), 5)
 gList = random.sample(range(46, 61), 5)
 oList = random.sample(range(61, 76), 5)
 
-def bottomTopLine():
-    print("*   *   *   *   *   *   *   *   *   *   *")
+# Make the board a dictionnary the player can interact with
+board = [
+    [
+        {"value": bList[i], "marked": False}, 
+        {"value": iList[i], "marked": False},
+        {"value": nList[i], "marked": False},
+        {"value": gList[i], "marked": False},
+        {"value": oList[i], "marked": False}
+    ]
+    for i in range(5)
+]
 
-def printBingoLineLess(count, bList, iList, nList, gList, oList):
-    print("*   " + str(bList[count]) + "   *   " + str(iList[count]) + "  *   " + str(nList[count]) + "  *   " + str(gList[count]) + "  *   " + str(oList[count]) + "  *")
+# Cursor position
+cursor_row, cursor_col = 0, 0
 
-def printBingoLineMore(count, bList, iList, nList, gList, oList):
-     print("*  " + str(bList[count]) + "   *   " + str(iList[count]) + "  *   " + str(nList[count]) + "  *   " + str(gList[count]) + "  *   " + str(oList[count]) + "  *")
+# Clear the console when updating the board
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def numberLine(bList, iList=None, nList=None, gList=None, oList=None):
-    count = 0
-    while(count < 5):
-            if (bList[count] < 10):
-                printBingoLineLess(count, bList, iList, nList, gList, oList)
+def print_board():
+    clear()
+    print("   B   I   N   G   O")
+    print("*" * 23)
+    for r in range(5):
+        row = ""
+        for c in range(5):
+            num = board[r][c]["value"]
+            if r == cursor_row and c == cursor_col:
+                # Highlight the selected number
+                if( board[r][c]["marked"]):
+                    row += f"{Fore.GREEN}[{str(num).rjust(2)}]{Style.RESET_ALL} " # Green (Selected and Marked)
+                else:
+                    row += f"[{str(num).rjust(2)}] " # Not green (Selected but not Marked)
             else:
-                printBingoLineMore(count, bList, iList, nList, gList, oList)    
-            count+= 1
+                if board[r][c]["marked"]:
+                    row += f" {Fore.GREEN}{str(num).rjust(2)}{Style.RESET_ALL}  " # Green (Marked)
+                else:
+                    row += f" {str(num).rjust(2)}  "  # Display the number normally
+        print(row)
+    print("*" * 23)
+    print("Use the arrow keys to move, Enter to select, ESC to quit")
 
-bottomTopLine()
+# Game Start!
+    
+print_board()
 
-numberLine(bList, iList, nList, gList, oList)
+# Main gameplay loop
 
-bottomTopLine()
+while True:
+
+    if keyboard.is_pressed('up') and cursor_row > 0:
+        cursor_row -= 1
+        print_board()
+        time.sleep(0.1)
+    elif keyboard.is_pressed('down') and cursor_row < 4:
+        cursor_row += 1
+        print_board()
+        time.sleep(0.1)
+    elif keyboard.is_pressed('left') and cursor_col > 0:
+        cursor_col -= 1
+        print_board()
+        time.sleep(0.1)
+    elif keyboard.is_pressed('right') and cursor_col < 4:
+        cursor_col += 1
+        print_board()
+        time.sleep(0.1)
+    elif keyboard.is_pressed('enter'):
+        board[cursor_row][cursor_col]['marked'] = not board[cursor_row][cursor_col]['marked']
+        print_board()
+        time.sleep(0.1)
+    elif keyboard.is_pressed('esc'):
+        break
